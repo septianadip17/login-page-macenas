52de5aa2fd6f857bb51b28255fa998022db41c97
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-12 14:17:12 +0700

    right image

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import "./App.css";
 import rumahSatu from "/public/img/rumah1.jpeg";
 
 function App() {
   return (
     <>
-    <div className="flex flex-col">
-      <div className="form w-1/3">
-        <h1>Sign in or create an account</h1>
+    <div className="container w-full flex min-h-screen items-start">
+      <div className="relative w-1/3">
+        <h1 className="flex text-3xl text-center">Sign in or create an account</h1>
         <p>Welcome back! Please enter your details.</p>
         <input type="text" />
         <input type="password" />
       </div>
-      <div className="image-header w-2/3">
-        <img src={rumahSatu} alt="Architectural House in front of a Pool" />
+      <div className="relative w-2/3 h-full flex flex-col">
+        <img className="w-full h-full object-cover rounded-sm" src={rumahSatu} alt="Architectural House in front of a Pool" />
       </div>
     </div>
     </>
