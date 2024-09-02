4df10011f3f2f239823c89339effaca901b6bab2
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-02 13:12:46 +0700

    create passwordn changed

--- src/pages/CreatePassword.jsx
+++ src/pages/CreatePassword.jsx
@@ -1,81 +1,38 @@
 import React from "react";
+import FormSection from "../components/FormSection";
+import CoverImageSection from "../components/CoverImageSection";
 import rumahDua from "../assets/img/rumah2.jpeg";
-import { GoArrowLeft } from "react-icons/go";
-import { GoArrowRight } from "react-icons/go";
+
 const CreatePassword = () => {
-  const bgStyle = {
-    backgroundImage: `url(${rumahDua})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-  };
-
   return (
-    <div className="w-full flex min-h-screen mx-auto">
-      {/* Form Section */}
-      <form className="flex items-center justify-center w-2/5 min-h-screen leading-10">
-        <div className="w-full p-28">
-          <h1 className="text-3xl font-medium mb-4">Create Password</h1>
-          <p className="mb-6 leading-5">
-            Use a minimm of 10 characters, including upperchase letters,
-            lowercase letters, and numbers.
-          </p>
-          <form>
-            <input
-              className="w-full h-10 p-2 mb-2 border rounded-s-sm"
-              type="password"
-              placeholder="Password"
-            />
-            <input
-              className="w-full h-10 p-2 mb-2 border rounded-s-sm"
-              type="password"
-              placeholder="Confirm Password"
-            />
-            <button
-              className="w-full py-2 mb-4 text-white font-medium bg-black hover:bg-gray-700"
-              type="button"
-            >
-              Continue
-            </button>
-          </form>
-
-          {/* Footer */}
-          <footer className="mt-4 flex items-center justify-center leading-5">
-            <p className="text-md text-center">
-              By signing in or creating an account, you agree to our{" "}
-              <span className="font-medium underline">Terms & Conditions</span>{" "}
-              and{" "}
-              <span className="font-medium underline">Privacy Statement</span>.
-            </p>
-          </footer>
-        </div>
-      </form>
-
-      {/* Cover Image Section */}
-      <div
-        style={bgStyle}
-        className="container w-3/5 rounded-xl flex flex-col justify-end m-2"
+    <main className="w-full flex min-h-screen mx-auto">
+      <FormSection
+        title="Create Password"
+        description="Use a minimum of 10 characters, including uppercase letters, lowercase letters, and numbers."
       >
-        <div className="inset-0 bg-black bg-opacity-50 flex items-center justify-center rounded-b-xl p-8">
-          <div className="text-white rounded-lg max-w-2xl">
-            <h1 className="text-3xl font-normal mb-4">
-              “Our stay at Hotel Macenas was absolutely delightful! From the friendly staff to clean and comfortable rooms, it trully exceeded our expectations.”
-            </h1>
-            <div className="flex items-center justify-between">
-              <span className="text-normal">Randy Bator • Founder & CEO</span>
-              <div className="flex space-x-4">
-                <button className="bg-secondary text-secondary-foreground p-2 rounded-full hover:bg-secondary/80">
-                  <GoArrowLeft className="w-6 h-6" />
-                </button>
-                <button className="bg-secondary text-secondary-foreground p-2 rounded-full hover:bg-secondary/80">
-                  <GoArrowRight className="w-6 h-6" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+        <input
+          className="w-full h-10 p-2 mb-2 border rounded-s-sm"
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          className="w-full h-10 p-2 mb-2 border rounded-s-sm"
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button
+          className="w-full py-2 mb-4 text-white font-medium bg-black hover:bg-gray-700"
+          type="button"
+        >
+          Continue
+        </button>
+      </FormSection>
+      <CoverImageSection
+        backgroundImage={rumahDua}
+        quote="Our stay at Hotel Macenas was absolutely delightful! From the friendly staff to clean and comfortable rooms, it truly exceeded our expectations."
+        author="Randy Bator • Founder & CEO"
+      />
+    </main>
   );
 };
 
