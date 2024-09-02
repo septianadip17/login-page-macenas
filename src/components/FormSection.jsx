fd500f0c3de6eee325d5e02e3f6215826abf19d6
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-02 14:57:47 +0700

    bordered contents

--- src/components/FormSection.jsx
+++ src/components/FormSection.jsx
@@ -3,11 +3,19 @@ import React from "react";
 
 const FormSection = ({ title, description, children }) => {
   return (
-    <form className="flex items-center justify-center w-2/5 min-h-screen leading-10">
-      <div className="w-full p-28">
+    <form className="flex items-center justify-center lg:w-2/5 min-h-screen leading-5 border-2 border-purple-500">
+      <div className="w-full p-28 border-2 border-yellow-400">
         <h1 className="text-3xl font-medium mb-4">{title}</h1>
         <p className="mb-6">{description}</p>
         {children}
+        <footer className="mt-4 flex items-center justify-center leading-5">
+          <p className="text-md text-center">
+            By signing in or creating an account, you agree to our{" "}
+            <span className="font-medium underline">Terms & Conditions</span>{" "}
+            and <span className="font-medium underline">Privacy Statement</span>
+            .
+          </p>
+        </footer>
       </div>
     </form>
   );
